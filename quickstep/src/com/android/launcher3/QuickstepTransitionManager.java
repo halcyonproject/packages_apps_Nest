412c2d1859555461330927050c92c464502fecb3
Pranav Vashi <neobuddy89@gmail.com>
2025-09-14 14:13:09 +0530

    Nest: Ensure surfaces/animators are cleaned in onActivityDestroyed()
    Change-Id: I1b976eed26d3f5b29275b827c80bfb24deac5463
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
+++ quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
@@ -1581,6 +1581,7 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
             mMoveDisplayTransition = null;
         }
         if (mBackAnimationController != null) {
+            mBackAnimationController.cleanupForDestroy();
             mBackAnimationController.unregisterBackCallbacks();
             mBackAnimationController.unregisterComponentCallbacks();
             mBackAnimationController = null;
