2ad442a08a2a47f921bfa986a1c02fc7c14aa868
Pranav Vashi <neobuddy89@gmail.com>
2026-03-04 00:20:28 +0530

    Revert "Nest: InvariantDeviceProfile: Do not hide grid options on Tablets"
    This reverts commit f9a6cf3b2c9996cf3c0d9ebf7ebb6c1374b5c0eb.
    
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -572,7 +572,8 @@ public class InvariantDeviceProfile {
 
     private static boolean firstGridFilter(GridOption gridOption, int deviceType,
             boolean allowDisabledGrid, boolean isFixedLandscapeMode) {
-        return (gridOption.isEnabled(deviceType) || allowDisabledGrid);
+        return (gridOption.isEnabled(deviceType) || allowDisabledGrid)
+                && gridOption.filterByFlag(deviceType, isFixedLandscapeMode);
     }
 
     private static List<DisplayOption> getPredefinedDeviceProfiles(
@@ -803,6 +804,7 @@ public class InvariantDeviceProfile {
         return parseAllDefinedGridOptions(context, displayInfo)
                 .stream()
                 .filter(go -> go.isEnabled(deviceType))
+                .filter(go -> go.filterByFlag(deviceType, isFixedLandscape))
                 .collect(Collectors.toList());
     }
 
@@ -1340,9 +1342,9 @@ public class InvariantDeviceProfile {
         public boolean isEnabled(@DeviceType int deviceType) {
             switch (deviceType) {
                 case TYPE_PHONE:
-                    return true;
+                    return (deviceCategory & DEVICE_CATEGORY_PHONE) == DEVICE_CATEGORY_PHONE;
                 case TYPE_TABLET:
-                    return true;
+                    return (deviceCategory & DEVICE_CATEGORY_TABLET) == DEVICE_CATEGORY_TABLET;
                 case TYPE_MULTI_DISPLAY:
                     return (deviceCategory & DEVICE_CATEGORY_MULTI_DISPLAY)
                             == DEVICE_CATEGORY_MULTI_DISPLAY;
